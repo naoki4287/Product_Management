b6ecc23a85990deb191b4cf7c6f4912469033270
github naoki4287 <github@waka.naoki86@icloud.com>
2022-07-19 17:25:16 +0900

    マイページにお気に入りを表示するようにしました

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -8,11 +8,36 @@
 
     <div class="mt-12 ml-4">
       <div>お気に入り</div>
-      <ul>
-        <li>コーラ</li>
-        <li>ファンタ</li>
-      </ul>
+      <table class="w-11/12 mx-auto my-0 mt-8 border-2 border-gray-300">
+        <thead>
+          <tr>
+            <th class="border-2 border-gray-200 px-4 py-2">ID</th>
+            <th class="border-2 border-gray-200 px-4 py-2">商品名</th>
+            <th class="border-2 border-gray-200 px-4 py-2">入荷元</th>
+            <th class="border-2 border-gray-200 px-4 py-2">製造元</th>
+            <th class="border-2 border-gray-200 px-4 py-2">金額</th>
+            <th class="border-2 border-gray-200 px-4 py-2">作成日</th>
+          </tr>
+        </thead>
+      @foreach ($favorites as $favorite)
+      <tbody>
+        <tr>
+          <td class="border-2 border-gray-200 px-4 py-2">{{ $favorite->product_id }}</td>
+          <td class="border-2 border-gray-200 px-4 py-2">{{ $favorite->product_name }}</td>
+          <td class="border-2 border-gray-200 px-4 py-2">{{ $favorite->arrival_source }}</td>
+          <td class="border-2 border-gray-200 px-4 py-2">{{ $favorite->manufacturer }}</td>
+          <td class="border-2 border-gray-200 px-8 py-2">{{ $favorite->price }}</td>
+          <td class="border-2 border-gray-200 px-4 py-2">{{ $favorite->created_at }}</td>
+        </tr>
+      </tbody>
+      @endforeach
+      </table>
     </div>
+
+
+    
+
+
 
     <div class="mt-12 ml-4">
       <div>操作履歴</div>
