3e477ca7d1cbd5f4433bce3c01c69c328831f892
github naoki4287 <github@waka.naoki86@icloud.com>
2022-07-19 17:22:35 +0900

    💄 リンクをクリックしやすいようにしました

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-  <div>
-    <a href="list">商品一覧</a><br>
-    <a href="newadd">商品新規追加</a><br>
-    <a href="contact">問い合わせ</a><br>
+  <div class="text-center text-2xl">
+    <a class="block mb-4" href="list">商品一覧</a><br>
+    <a class="block mb-4" href="newadd">商品新規追加</a><br>
+    <a class="block mb-4" href="contact">問い合わせ</a><br>
   </div>
 </x-app-layout>
