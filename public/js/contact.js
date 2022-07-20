f34dee30692b0a5c88bf51f54db3648e90bcc6fa
github naoki4287 <github@waka.naoki86@icloud.com>
2022-07-20 20:17:37 +0900

    ✨ 問い合わせフォームを回収しました。

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,6 +1,25 @@
 "use strict";
 
-$("#syumi").hide();
-$('male').on('click', () => {
+$(function () {
+    $("#male").on("click", function () {
+        $("#syumi").attr("name", "syumi")
+          .attr('placeholder', '趣味');
+    });
+});
 
-})
+$(function () {
+    $("#female").on("click", function () {
+        $("#syumi").attr("name", "tokugi")
+          .attr('placeholder', '特技');
+    });
+});
+
+$(function () {
+    $(".check").on("click", function () {
+        if ($(this).prop("checked")) {
+            $(".check").prop("checked", false);
+            $(this).prop("checked", true);
+        }
+    });
+});
+
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,23 +14,14 @@
       <label class="inline-block mt-4" for="tel">電話番号</label><br>
       <x-PMinput name="tel" value="{{ old('tel') }}"></x-PMinput><br>
       <label class="inline-block mt-4" for="gender">男性</label>
-      <input class="mr-8" type="checkbox" name="gender" id="male" checked><br>
-      <label class="inline-block mt-4" for="syumi">趣味</label><br>
-      <textarea class="w-4/12 rounded-md border-gray-300" name="syumi" rows="5" id="syumi"></textarea><br>
+      <input class="mr-8 check" type="checkbox" name="male" id="male" checked>
       <label class="inline-block mt-4" for="gender">女性</label>
-      <input type="checkbox" name="gender" id="female"><br>
+      <input class="check" type="checkbox" name="female" id="female"><br>
+      <textarea class="w-4/12 rounded-md border-gray-300" name="syumi" rows="5" id="syumi" placeholder="趣味"></textarea><br>
       <label class="inline-block mt-4" for="contact">問い合わせ内容</label><br>
       <textarea class="w-4/12 rounded-md border-gray-300" name="contact" rows="5">{{ old('contact') }}</textarea><br>
       <x-button class="mt-8">送信</x-button>
     </form>
   </div>
-  {{-- <script src="{{ mix('js/jquery.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('js/jquery.min.js') }}"></script> --}}
   <script src="{{ asset('js/contact.js') }}"></script>
-  <script>
-    // $("#syumi").hide();
-    // $('male').on('click', () => {
-
-    // })
-  </script>
 </x-app-layout>
