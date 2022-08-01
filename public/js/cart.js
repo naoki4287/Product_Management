8f609864a726fb217583e20b55290c73e8032fa7
github naoki4287 <github@waka.naoki86@icloud.com>
2022-08-01 13:39:10 +0900

    cartページのレイアウトを作成し、JSでcartの商品の情報を生じできるようにしました。

---
+++ public/js/cart.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const itemSum = document.getElementsByClassName("itemSum");
+const sum = document.getElementById("sum");
+const cartInItemSum = document.getElementById("cartInItemSum");
+const priceSum = Array.from(itemSum);
+
+{
+    const num = items.map((item) => {
+        let sum = item.item_num;
+        return sum;
+    });
+
+    const quantity = num.reduce((sum, num) => {
+        return sum + num;
+    });
+    console.log(quantity);
+    cartInItemSum.textContent = `合計：${quantity}個`;
+}
+
+{
+    const cartSum = items.map((item) => {
+        let sum = item.price * item.item_num;
+        return sum;
+    });
+
+    const total = cartSum.reduce((sum, price) => {
+        return sum + price;
+    }, 0);
+
+    const totalComma = total.toLocaleString();
+    sum.textContent = `￥${totalComma}円`;
+}
+
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,45 @@
 <x-app-layout>
   <div>
-    @foreach ($cartInItems as $cartInItem)
-      <div>{{ $cartInItem->product_name }}</div>
-    @endforeach
+    <div class="text-center text-2xl mt-8">ショッピングカート</div>
+
+    <div class="">
+      @foreach ($cartInItems as $cartInItem)
+      <div class="border-2 border-white p-1 mt-8 w-11/12 mx-auto my-0">
+        <label>商品名：</label>
+        <div class="inline-block font-bold">{{ $cartInItem->product_name }}</div><br>
+        <label>商品単価：</label>
+        <div class="inline-block ">{{ $cartInItem->price }}</div><br>
+        <label>個数：</label>
+        <div class="inline-block ">{{ $cartInItem->item_num }}</div><br>
+        <label>商品合計:</label>
+        <div class="itemSum inline-block">{{ $cartInItem->price * $cartInItem->item_num }}</div>
+        <form action="" method="POST">
+          @csrf
+          <div class="flex justify-end">
+            <x-button class="bg-red-700 hover:bg-red-600 px-2 py-1">削除</x-button>
+          </div>
+        </form>
+      </div>
+      @endforeach
+    </div>
+
+    <div class="">
+      <div class="border-2 border-indigo-700 text-center p-1 mt-8 w-6/12 mx-auto my-0 py-4">
+        <div>{{ count($cartInItems) }}点の商品</div>
+        <div id="cartInItemSum"></div>
+        <form action="" method="POST">
+          @csrf
+          <div id="sum"></div>
+          <div>
+            <x-button class="bg-yellow-300 hover:bg-yellow-200 text-gray-900 mt-4 px-16">購入</x-button>
+          </div>
+        </form>
+      </div>
+    </div>
+
   </div>
+  <script type="module" src="{{ asset('js/cart.js') }}"></script>
+  <script>
+    const items = @json($cartInItems);
+  </script>
 </x-app-layout>
