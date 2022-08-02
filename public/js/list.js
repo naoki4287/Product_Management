a6bbb1b16966199482cdc5954265c56064d9709c
github naoki4287 <github@waka.naoki86@icloud.com>
2022-08-02 10:22:14 +0900

    🔥

--- public/js/list.js
+++ public/js/list.js
@@ -9,7 +9,6 @@ const checkbox = document.getElementsByName("checkbox");
 const checkboxes = Array.from(checkbox);
 const items = itemsList.data;
 const selects = Array.from(select);
-console.log(items);
 
 delModalOpen.addEventListener("click", () => {
     deleteModal.classList.remove("hidden");
@@ -25,24 +24,17 @@ $(".checkbox").on("click", function () {
         let id = $(this).attr("value");
         $(this).attr("id", id).attr("disabled", true);
         cartItemId.value += id + ",";
-        console.log(cartItemId.value);
     } else {
         $(this).removeAttr("id").removeAttr;
     }
 });
 
 $(".select").on("change", function () {
-    console.log($(this));
     if ($(this).val() !== "" && $(this).attr("id") === undefined) {
         let id = $(this).val();
         $(this).attr("id", id);
     }
-    console.log($(this).attr("id"));
 });
-
-for (let i = 0; i < select.length; i++) {
-    console.log(select[i].value);
-}
 
 $("#cartBtn").on("click", function () {
     const selectedItems = selects.filter((select) => {
