5b38cfb4ebdcc556eb8710fd0bf3fa446e197230
github naoki4287 <github@waka.naoki86@icloud.com>
2022-08-04 15:24:38 +0900

    jQueryの読み込み方を変えました

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
  
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="js/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     </head>
     <body class="font-sans antialiased">
