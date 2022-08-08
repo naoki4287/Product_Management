ad719e4063de9e84744360fe51a042fb1220665f
github naoki4287 <github@waka.naoki86@icloud.com>
2022-08-08 12:02:11 +0900

    🔥

--- resources/views/shippingDestination/register.blade.php
+++ resources/views/shippingDestination/register.blade.php
@@ -7,12 +7,11 @@
         @endforeach
       </ul>
       <label class="inline-block mt-4" for="name">出荷先会社名</label><br>
-      <x-PMinput class="input" name="name" id="name" value="{{ old('name') }}" required></x-PMinput><br>
+      <x-PMinput class="input" name="name" id="name" value="{{ old('name') }}"></x-PMinput><br>
       <label class="inline-block mt-4" for="address">出荷先住所</label><br>
-      <x-PMinput class="input" name="address" id="address" value="{{ old('address') }}" required></x-PMinput><br>
+      <x-PMinput class="input" name="address" id="address" value="{{ old('address') }}"></x-PMinput><br>
       <label class="inline-block mt-4" for="tel">出荷先電話番号</label><br>
-      {{-- <x-PMinput class="input" name="tel" id="tel" value="{{ old('tel') }}"></x-PMinput><br> --}}
-      <x-PMinput class="input" type="tel" id="tel" name="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required /><br>
+      <x-PMinput class="input" name="tel" id="tel" value="{{ old('tel') }}"></x-PMinput><br>
       <x-button class="mt-8" id="registerBtn">送信</x-button>
     </div>
     <div id="confirmPage">
