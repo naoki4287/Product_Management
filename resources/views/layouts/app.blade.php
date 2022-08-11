766ccbfb938cce9ff1c0ebb953eebb2e77843b7e
github naoki4287 <github@waka.naoki86@icloud.com>
2022-08-11 18:24:56 +0900

    🔥

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
 
   <!-- Styles -->
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/bg.css') }}">
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
